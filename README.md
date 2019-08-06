3c0d9735559cdea4e8e284961ea27edac7f384d7
platformcoders <53493479+platformcoders@users.noreply.github.com>
2019-08-05 17:01:16 -0700

    Updates
    Updated readme and Powershell files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project vmmshutfix
+# Virtual Machine Preshutdown Notification Service
 This project is for Virtual Machine Preshutdown Notification Service.  Service sends Preshutdown notification message to Virtual Machines ahead of Hyper-V Managemnt Services.  
 THis was originally written to solve issue with a Server 2019 HyperV bug where during the host restart machines were being turn it off instead of shutdown. With this service and machines set to "Save" system will attempt to shutdown VM's and those that dont complete shutdown on time will be saved at the point where it no longer matter for them because they were in a process comencing shtdown anyway. This is important for domain controllers, Database servers, Exchanges servers and any machine running replication. 
 
@@ -10,10 +10,10 @@ Download This project from github as zip file
 * [Github ZIP] (https://github.com/platformcoders/vmmshutfix/archive/master.zip)
 
 Compiled version can be downloaded from following location
-* [Githubrelease]
+* [Github release] (https://github.com/platformcoders/vmmshutfix/releases/tag/vmmshytfix)
 
 ## Installing
-1. Unzip files to a folder and right click on **servicesettingsscript_V1.ps1**  than select edit. 
+1. Unzip downloaded files to a folder and right click on **servicesettingsscript_V1.ps1**  than select edit. 
 2. This should open Powershell ISE, click on the Run script icon or press F5 to run script. 
 3. Powershell supposed to create folder **vmmshutfix** under **c:\Program Files** and copy 
 * vmmshutfix.exe
@@ -25,5 +25,12 @@ hklm:SYSTEM\CurrentControlSet\Control
 4. Type **start-service vmmshutfix** at PowerShell prompt, and watch for confirmation if service is started.
 5. Once service is started you can set your Virtual Machines to **Auto Save** on Host reboots. 
 
+
+## Uninstalling 
+1. Open CMD as an Administrator
+2. Type **Net Stop vmmshutfix** and press **Enter**
+3. Once service stops, than Type **sc.exe delete vmmshutfix.exe** and press **Enter**
+4. You should get confirmation that service is deleted 
+5. Now you can delete **vmmshutfix** directory under **C:\Program Files**
 
 
--- servicesettingsscript_V1.ps1
+++ servicesettingsscript_V1.ps1
@@ -1,6 +1,32 @@
-﻿$Stringarray = @()
+﻿<#
+//The MIT License (MIT) 
+// 
+//
+// 
+//Permission is hereby granted, free of charge, to any person obtaining a copy 
+//of this software and associated documentation files (the "Software"), to deal 
+//in the Software without restriction, including without limitation the rights 
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+//copies of the Software, and to permit persons to whom the Software is 
+//furnished to do so, subject to the following conditions: 
+// 
+//The above copyright notice and this permission notice shall be included in 
+//all copies or substantial portions of the Software. 
+// 
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+//THE SOFTWARE. 
+// 
+
+#>
+
+$Stringarray = @()
 $newarray = @()
-$value = get-ItemProperty -Path "hklm:SYSTEM\CurrentControlSet\Control" -Name PreshutdownOrder #|  select -expandproperty preshutdownorder # -Value $a
+$value = get-ItemProperty -Path "hklm:SYSTEM\CurrentControlSet\Control" -Name PreshutdownOrder 
 $Stringarray = $value.Preshutdownorder
 
 
@@ -32,9 +58,7 @@ $newarray
 
 
 
-#Set-ItemProperty -Path "hklm:SYSTEM\CurrentControlSet\Control" -Name "WaitToKillServiceTimeout" -type DWord -Value "5000"
 
-#Set-ItemProperty -Path "hklm:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization" -Name "ShutdownTimeout" -type DWord -Value "120"
 
 $findvmmshutfix = get-service -name vmmshutfix -ErrorAction SilentlyContinue
 
@@ -61,7 +85,7 @@ New-Item -Path $despath -ItemType Directory
 
 
 
-#copy files to system32
+
 Copy-Item -Path ".\vmmshutfix.exe" -Destination "$despath" -force
 copy-item -path ".\vmmshutfix.exe.config"  -Destination "$despath" -force
 copy-item -path ".\vmmshutfix.pdb"  -Destination "$despath" -force
